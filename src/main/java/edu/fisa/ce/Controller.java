b965da9555ff209e20f6d05be2306c734e42b4ae
Soooonnn <ksj386502@gmail.com>
2026-03-24 15:37:49 +0900

    jar수정

--- src/main/java/edu/fisa/ce/Controller.java
+++ src/main/java/edu/fisa/ce/Controller.java
@@ -8,7 +8,7 @@ public class Controller {
 	
 	@GetMapping("ce1")
 	public String getDate() {
-		System.out.println("요청 및 응답(수정 전) -----------");
+		System.out.println("요청 및 응답(수정 후111) -----------");
 		return "응답 데이터";
 	}
 
